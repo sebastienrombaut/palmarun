1f59491167c2148a5106e943a4fc418dc75e7ef6
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2019-09-15 20:39:03 +0200

    Write first tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  validates_presence_of :first_name, :last_name
+  validates_uniqueness_of :email
 end
