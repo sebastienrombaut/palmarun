23bee1745eb44d938b7f32fa2a7ead263b5ed609
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2019-09-28 19:32:19 +0200

    Create index page with table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if current_user
-      @results = current_user.results
+      @results = current_user.results.joins(:race).order("races.date DESC")
     end
   end
 end
