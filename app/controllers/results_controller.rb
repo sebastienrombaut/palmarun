fe089a339bdeb49af4aea2c85d4f083d41caa866
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2019-09-29 21:49:17 +0200

    Result creation

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,14 +4,44 @@ class ResultsController < ApplicationController
   end
 
   def new
+    @result = Result.new
+    @races_names = Race.all.map { |r| r.name }
+  end
+
+  def create
+    @result = Result.new
+    @result.running_time = convert_time_input_into_seconds_integer(params[:running_time])
+    @result.race_id = Race.find_by(name: params[:race_name]).id
+    @result.user_id = current_user.id
+    @result.general_classification = params[:general_classification]
+
+    if @result.save
+      flash[:success] = "Le résultat a bien été crée !"
+      redirect_to root_path
+    else
+      flash[:danger] = "Le résultat n'a pas été crée"
+      redirect_to new_result_path
+    end
   end
 
   def show
   end
 
-  def create
+  def destroy
   end
 
-  def destroy
+  private
+
+  def result_params
+    params.require(:result).permit(
+      :race_id,
+      :user_id,
+      :general_classification,
+      :running_time,
+    )
+  end
+
+  def convert_time_input_into_seconds_integer(string)
+    string.split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -3,4 +3,5 @@ class Result < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :user_id, :race_id, :running_time
+  validates :running_time, numericality: { other_than: 0 }
 end
