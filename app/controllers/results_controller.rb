79ec8658276a3d892b28f9e4395cc63f4000b839
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2019-12-08 20:21:50 +0100

    Add destroy results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -36,17 +36,26 @@ class ResultsController < ApplicationController
 
     if @result.update(result_params)
       flash[:success] = "Le résultat a bien été édité !"
-      redirect_to root_path
     else
       flash[:danger] = "Le résultat n'a pas été édité"
-      redirect_to root_path
     end
+
+    redirect_to root_path
   end
 
   def show
   end
 
   def destroy
+    @result = Result.find(params[:id])
+
+    if @result.destroy
+      flash[:success] = "Le résultat a bien été supprimé !"
+    else
+      flash[:danger] = "Le résultat n'a pas été supprimé"
+    end
+
+    redirect_to root_path
   end
 
   private
